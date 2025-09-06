e7bf5b33c49afbca829f0eaa06495c4029262333
FaseehNovice <faseehurrehman499@gmail.com>
2025-09-06 02:54:16 -0700

    functions to select and return choice for both computer and Human

--- javascript.js
+++ javascript.js
@@ -1,1 +1,26 @@
-console.log("Hello, world!");
+
+function getComputerChoice(){
+    let compChoice = Math.floor((Math.random() * 3)+1);
+    if(compChoice == 1){
+        return "Rock"
+    }
+    else if(compChoice == 2){
+        return "Paper"
+    }
+    else{
+        return "Scissors"
+    }
+}
+
+function getHumanChoice(){
+    let humanChoice = parseInt(prompt("Choose\n1: Rock\n2: Paper\n3: Scissors "))
+    if(humanChoice == 1){
+        return "Rock"
+    }
+    else if(humanChoice == 2){
+        return "Paper"
+    }
+    else{
+        return "Scissors"
+    }
+}

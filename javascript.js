c940795f42dec4618ffe262d0dd2f6341b53b28b
FaseehNovice <faseehurrehman499@gmail.com>
2025-09-12 20:34:56 -0700

    add divs to show results and complete a basic ui Rock paper and Scissors

--- javascript.js
+++ javascript.js
@@ -48,6 +48,11 @@ function playRound(humanChoice , compChoice){
 }
 
 let buttons = document.querySelectorAll("button");
+let container = document.querySelector("#container")
+let humanSelDiv = document.createElement("div");
+let compSelDiv = document.createElement("div");
+let matchResult = document.createElement("div");
+let finalResult = document.createElement("div");
 
 let humanSelection;
 let computerSelection;
@@ -56,6 +61,7 @@ buttons.forEach((button) => {
     button.addEventListener("click",() => {
         console.clear();
         humanSelection = button.id;
+        finalResult.textContent = "";
         computerSelection = getComputerChoice();
 
         console.log("**************************************");
@@ -63,25 +69,36 @@ buttons.forEach((button) => {
 
         console.log("--------------------------------------");
 
-        console.log(`You Picked ${humanSelection}`);
-        console.log(`Computer Picked ${computerSelection}`);
+        //console.log(`You Picked ${humanSelection}`);
+        humanSelDiv.textContent = `You Picked ${humanSelection}`;
+        
+        //console.log(`Computer Picked ${computerSelection}`);
+        compSelDiv.textContent = `Computer Picked ${computerSelection}`;
 
         console.log("--------------------------------------");
 
-        console.log(`You Won ${humanScore} matches`);
-        console.log(`Your Opponent Won ${computerScore} matche(s)`);
+        //console.log(`You Won ${humanScore} matches`);
+        //console.log(`Your Opponent Won ${computerScore} matche(s)`);
+        matchResult.textContent = `You Won ${humanScore} matche(s)\nYour Opponent Won ${computerScore} matche(s)`;
 
         if (humanScore === 5 || computerScore === 5) {
             if (humanScore > computerScore) {
-                console.log("🎉 Congrats You Won the matchup!");
+                //console.log("🎉 Congrats You Won the matchup!");
+                finalResult.textContent = "Congrats You Won the matchup!";
             } else {
-                console.log("💻 You Lost. Better Luck next time!");
+                //console.log("💻 You Lost. Better Luck next time!");
+                finalResult.textContent = "Congrats You Won the matchup!";
             }
+
         humanScore = 0;
         computerScore = 0;
         
         console.log("Scores have been reset. Play again!");
         }   
+        container.appendChild(humanSelDiv);
+        container.appendChild(compSelDiv);
+        container.appendChild(matchResult);
+        container.appendChild(finalResult);
     })
 })
 
